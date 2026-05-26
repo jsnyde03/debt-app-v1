c2f82d3fd3990581c39eaef30d896c98992c54ca
Jason Snyder <jason.snyder1984@gmail.com>
2026-05-25 22:41:58 -0400

    Add capacitor mobile shell and Codemagic workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jasonsnyder.debtplanner',
+  appName: 'Debt Planner',
+  webDir: 'out'
+};
+
+export default config;
