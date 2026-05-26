b8a981e3981d24bf7f1d550cd0ab405e0e95e236
Jason Snyder <jason.snyder1984@gmail.com>
2026-05-26 17:00:30 -0400

    Fix iOS status bar safe area

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.jasonsnyder.debtplanner',
   appName: 'Debt Planner',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+      style: "DARK",
+      backgroundColor: "#eef3f8"
+    }
+  }
 };
 
 export default config;
